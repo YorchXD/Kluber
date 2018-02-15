e5fc72a7877cfba2c0ca567f739ce7c1616001cb
Unknown <yorch5.77@gmail.com>
2018-02-15 13:24:43 -0300

    Página ejecutivo
     Login y menu

---
+++ PaginaEjecutivo/Principal.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" href="stylePrincipal.css">
+	<link rel="stylesheet" type="text/css" href="Imagenes\fonts.css">
+</head>
+<body>
+	<header>
+		<nav class="menu">
+			<ul>
+				<li><a href="#"><span class="colorInicio"><i class="icon icon-home"></i></span>Inicio</a></li>
+				<li><a href="#"><span class="colorHistorial"><i class="icon icon-open-book"></i></span>Historial</a></li>
+				<li><a href="#"><span class="colorChofer"><i class="icon icon-person_pin"></i></span>Chofer</a>
+					<ul>
+						<li><a href="#" class="colorChofer">Registrar</a></li>
+						<li><a href="#" class="colorChofer">Editar</a></li>
+						<li><a href="#" class="colorChofer">Eliminar</a></li>
+					</ul>
+				</li>
+				<li><a href="#"><span class="colorTaxi"><i class="icon icon-local_taxi"></i></span>Taxi</a>
+					<ul>
+						<li><a href="#">Registrar</a></li>
+						<li><a href="#">Editar</a></li>
+						<li><a href="#">Eliminar</a></li>
+					</ul>
+				</li>
+			</ul>				
+		</nav>
+	</header>
+
+</body>
+</html>
